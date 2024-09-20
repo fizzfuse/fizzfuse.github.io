74dd88a449f6fff59f8e82dab761ec32aae983e4
rflxtns <136486432+rflxtns@users.noreply.github.com>
2024-09-20 18:59:05 -0400

    Update countdown.js

---
+++ assets/js/countdown.js
@@ -0,0 +1,19 @@
+const countDownDate = new Date("Sep 21, 2024 02:00:00").getTime();
+
+const x = setInterval(function() {
+    const now = new Date().getTime();
+    const distance = countDownDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("countdown").innerHTML = days + "D " + hours + "H "
+    + minutes + "M " + seconds + "S ";
+
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("countdown").innerHTML = "EVENT INITIATED";
+    }
+}, 1000);
