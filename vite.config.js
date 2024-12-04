85ac8fbdcfd275abd7cc0d881a24a8685e2a01ec
mblizzy <83024765+mblizzy@users.noreply.github.com>
2024-12-04 15:36:35 -0500

    Initial

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: process.env.NODE_ENV === 'production' ? '/fizzfuse.github.io/' : '/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  optimizeDeps: {
+    exclude: ['fsevents']
+  },
+  server: {
+    watch: {
+      ignored: ['**/node_modules/**']
+    }
+  }
 })
